948a432d2f1ace0c8f4596b6a81908123bd4db93
Jordan Moureau <jordanfromnovember@gmail.com>
2026-09-15 11:02:06 -0700

    G4 tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
+
 import "./globals.css";
 import "./styles/hero.css";
 import "./styles/navbarmain.css";
@@ -34,6 +36,21 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-5H1FDLJ2KV"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-5H1FDLJ2KV');
+          `}
+        </Script>
+
         <Topper />
         <NavBar />
         <NavBarMobile />
